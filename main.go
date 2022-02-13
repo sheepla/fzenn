3efd181401b51efb3f0f1e3cfc8789268cb9a771
Sheepla <62412884+sheepla@users.noreply.github.com>
2022-02-13 16:25:01 +0900

    Fix main.go

--- main.go
+++ main.go
@@ -1,10 +1,8 @@
-/*
-Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-
-*/
 package main
 
-import "github.com/sheepla/fzenn/cmd"
+import (
+	"github.com/sheepla/fzenn/cmd"
+)
 
 func main() {
 	cmd.Execute()
